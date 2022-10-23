2c97f6ab7e4266ad7b8b5d43e9b7e46fb5d76c09
Roaa-Hussam <roaahussam911@gmail.com>
2022-10-23 20:00:15 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { useEffect, useState } from "react"
+
+// create your App component here
+export default function App() {
+
+    const [image, setImage] = useState(null)
+    useEffect(() => {
+
+        const fetchData = async () => {
+            const response = await fetch('https://dog.ceo/api/breeds/image/random')
+            const data = await response.json()
+            setImage(data.message)
+        }
+        fetchData()
+
+    }, [])
+
+    if (!image) return <p>Loading</p>
+    return <img src={image} alt="A Random Dog" />
+
+
+}
